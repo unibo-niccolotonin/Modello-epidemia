4130fa8c559f5b7b703502a6f4cbf242454bea5c
Niccolò Tonin <niccolo.tonin@studio.unibo.it>
2020-05-02 12:18:14 +0200

    Continuato con la funzione infection

--- rules.hpp
+++ rules.hpp
@@ -9,10 +9,9 @@ namespace infection_constants
     const double infection_probability = 0.1;
 }
 
-auto check_radius(Board& board, int y, int x, int radius)
+auto get_people_inside_radius(Board& board, int const& y, int const& x, int const& radius)
 {
-    std::vector<Person*> nearby_people; //Da sostituire con l'unique pointer
-
+    std::vector<Person> nearby_people; //Da sostituire con l'unique pointer
     for (int i = 0; i < (2 * radius + 1); i++)
     {
         for (int j = 0; j < (2 * radius + 1); j++)
@@ -23,26 +22,35 @@ auto check_radius(Board& board, int y, int x, int radius)
             if (person != NULL) nearby_people.push_back(person);
         }
     }
-
+    auto central_person_iterator = std::find(nearby_people.begin(), nearby_people.end(), board.get_person(y, x));
+    nearby_people.remove(central_person_iterator);
     return nearby_people;
 }
 
 //Pensare metodo di board che contiene le persone
-void infection(Board& board, std::vector<Person> &persons)
+void infection(Board& board)
 {
-    for (auto current_person : persons)
+    for (auto current_person : board.persons)
     {
-        if (a.get_status == status::INFECTED)
+        if (current_person.get_status == status::INFECTED)
         {
-            auto nearby_people = check_radius(board, current_person.get_x(), current_person.get_y(), infection_constants::radius);
+            auto nearby_people = get_people_inside_radius(board, current_person.get_y(), current_person.get_x(), infection_constants::radius);
             //Da completare
+
+            for (auto infection_candidate : nearby_people)
+            {
+                // controllare che è veramente casuale
+                std::random_device e;
+                std::uniform_real_distribution<double> distribution(0,1);
+                double random = distribution(e);
+            }
         }
     }
 }
 
-void healing()
+void healing(Person& person)
 {
-    
+    if()
 }
 
 void evolve(Board& board)
