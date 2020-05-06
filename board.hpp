d904b088568262908ac41077bb4a220c3ef007a7
unibo-elenaT <elena.tabakovic@studio.unibo.it>
2020-05-06 17:20:40 +0200

    board2

--- board.hpp
+++ board.hpp
@@ -1,27 +1,51 @@
 // Type your code here, or load an example.
 #include <vector>
 #include<iostream>
-
 class Cell
 {
     int n;
-    bool state;
 public:
-    Cell(bool state_) : state{ state_ } {}
-
-    bool statocellula();
+    enum state { alive, dead, immune, non_existant };
+    int Vx, Vy;
+    int l, c; //coordinate
 };
-bool Cell::statocellula()
-{
-    return state;
-}
+
 
 class Board
 {
-    bool st;
     int size;
-    std::vector<std::vector<bool>> v1;
+    std::vector<std::vector<Cell>> board_;
 public:
-    Board(int n) : size{ n }, v1(n, std::vector<bool>(n)) {}
+    Board(int n) : size{ n }, board_(n, std::vector<Cell>(n, state::non_existant)) {}
     int size_() const { return size; }
+    state insertCell(int r, int c);
 };
+
+state Board::insertCell(int r, int c)
+{
+    if (r < size && c < size)
+    {
+        if (state::non_existant)
+        {
+            return state::alive;
+        }
+        if (state::dead)
+        {
+            return state::alive;
+        }
+        if (state::alive)
+        {
+            state::non_existant;
+        }
+    }
+    else
+    {
+        return state::non_existant;
+    }
+}
+int main()
+{
+    int dim;
+    dim = 10;
+    Board board(dim);
+}
