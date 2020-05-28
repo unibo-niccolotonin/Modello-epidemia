778758cbd791353b32a9038f9585ba00a6d0d230
unibo-elenaT <elena.tabakovic@studio.unibo.it>
2020-05-28 10:18:50 +0200

    modifica 2

--- board.hpp
+++ board.hpp
@@ -1,15 +1,17 @@
 // Type your code here, or load an example.
 #include <vector>
 #include<iostream>
-enum state { alive, dead, immune, non_existant, infected };
+enum state { susceptible, dead, immune, non_existant, infected };
 class Cell
 {
     int n;
 public:
     int Vx, Vy;
     state status;
-    Cell(state stato) : status{ stato } {}
+    Cell(state stato) : status{ stato } {};
 };
+
+
 
 
 class Board
@@ -18,27 +20,32 @@ class Board
     std::vector<std::vector<Cell>> board_;
 public:
 
-    Board(int n) : size{ n }, board_(n, std::vector<Cell>(n, Cell(non_existant))) {}
-    int size_() const { return size; }
+    Board(int n) : size{ n }, board_(n, std::vector<Cell>(n, Cell(state::non_existant))) {};
+    int get_size() const { return size; }
     bool insertCell(int r, int c);
+
+    Cell& operator()(int x, int y)
+    {
+        return board[x][y];
+    }
+
+    Cell operator()(int x, int y) const
+    {
+        return Board[x][y];
+    }
+
 };
 
 bool Board::insertCell(int r, int c)
 {
-    if (r < size && c < size)
+    if (r >= 0 && c >= 0 && r < size && c < size)
     {
-        if (board_[r][c].status == non_existant)
+        if (board_[r][c].status == state::non_existant)
         {
-            board_[r][c].status == alive;
+            board_[r][c].status = state::susceptible;
             return true;
         }
         else
             return false;
     }
-}
-int main()
-{
-    int dim;
-    dim = 10;
-    Board board(dim);
 }
