797232cc62e399b47ced4e0bb31ab901c1f4d3f0
unibo-elenaT <elena.tabakovic@studio.unibo.it>
2020-05-28 11:29:13 +0200

    modifica 9

--- board.hpp
+++ board.hpp
@@ -8,9 +8,9 @@ class Cell
     int n;
 public:
     int Vx, Vy;
-    int clock = 0;
+    int clock;
     state status;
-    Cell(state stato) : status{ stato } {};
+    Cell(state stato) : status(stato), Vx(0), Vy(0), clock(0) {};
 };
 
 
@@ -22,13 +22,13 @@ class Board
     std::vector<std::vector<Cell>> board_;
 public:
 
-    Board(int n) : size{ n }, board_(n, std::vector<Cell>(n, Cell(state::non_existant))) {};
+    Board(int n) : size(n), board_(n, std::vector<Cell>(n, Cell(state::non_existant))) {};
     int get_size() const { return size; }
     bool insertCell(int r, int c);
 
     Cell& operator()(int x, int y)
     {
-        assert(x >= 0 && x < size && y >= 0 && y < size);
+        assert(x >= 0 && x < size&& y >= 0 && y < size);
         return board_[x][y];
     }
 
@@ -39,6 +39,20 @@ public:
     }
 
 };
+
+bool Board::insertCell(int r, int c)
+{
+    if (r >= 0 && c >= 0 && r < size && c < size)
+    {
+        if (board_[r][c].status == state::non_existant)
+        {
+            board_[r][c].status = state::susceptible;
+            return true;
+        }
+        else
+            return false;
+    }
+}
 
 bool Board::insertCell(int r, int c)
 {
