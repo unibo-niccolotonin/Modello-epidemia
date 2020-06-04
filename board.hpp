b38e763d4bc1feb5578fd2031f0ae5d7c49343eb
unibo-elenaT <elena.tabakovic@studio.unibo.it>
2020-06-04 16:11:35 +0200

    aggiunta funzione di insertcell n 2

--- board.hpp
+++ board.hpp
@@ -25,6 +25,7 @@ public:
     Board(int n) : size(n), board_(n, std::vector<Cell>(n, Cell(state::non_existant))) {};
     int get_size() const { return size; }
     bool insertCell(int r, int c);
+    bool insertCell(int r, int c, state statocellula);
 
     Cell& operator()(int x, int y)
     {
@@ -51,6 +52,24 @@ bool Board::insertCell(int r, int c)
         }
         else
             return false;
-    } else
-    return false;
+    }
+    else
+        return false;
+};
+
+bool Board::insertCell(int r, int c, state statocellula)
+{
+    if (r >= 0 && c >= 0 && r < size && c < size)
+    {
+        if (board_[r][c].status == state::non_existant)
+        {
+            board_[r][c].status = statocellula;
+            return true;
+        }
+        else
+            return false;
+    }
+    else
+        return false;
 }
+
