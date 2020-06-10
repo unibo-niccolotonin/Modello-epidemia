e4f7a7c06b5404f948ffae3ea1497ceaa7e0e8ec
Niccolò Tonin <niccolo.tonin@studio.unibo.it>
2020-06-10 17:31:10 +0200

    Pulizia dei commenti e delle variabili

--- evolve.cpp
+++ evolve.cpp
@@ -5,60 +5,60 @@
 
 namespace infection_constants
 {
-    int const radius = 5;
-    float const infection_probability = 1;
+    int const radius = 3;
+float const infection_probability = 0.5;
     int const infection_time = 30;
-    double mortality_weight = 0.2;
+    double mortality_rate = 0.2;
 }
 
 Board evolve(Board const& board)
 {
     Board new_board = board;
-
+    
+    std::random_device e;
+    std::uniform_real_distribution<double> distribution_for_probability(0, 1);
+    std::uniform_int_distribution<int> velocity_distribution(-1, 1);
+    
     for (int row = 0; row < board.get_size(); row++)
     {
         for (int column = 0; column < board.get_size(); column++)
         {
             Cell current_cell = board(row, column);
-
-            //infection
+            
+            //Cellule infette
             if (current_cell.status == state::infected)
             {
-                //Infect other cells
+                
+                //Infetta le altre cellule dentro il raggio
                 for (int i = 0; i < (2 * infection_constants::radius + 1); i++)
                 {
                     for (int j = 0; j < (2 * infection_constants::radius + 1); j++)
                     {
-                        int coord_x = (row - infection_constants::radius) + i;
-                        int coord_y = (column - infection_constants::radius) + j;
+                        int coord_y = (row - infection_constants::radius) + i;
+                        int coord_x = (column - infection_constants::radius) + j;
 
-                        if (coord_x >= 0 && coord_y >= 0 && coord_x < board.get_size() && coord_y < board.get_size())
+                        if (coord_y >= 0 && coord_x >= 0 && coord_y < board.get_size() && coord_x < board.get_size())
                         {
-                            if (new_board(coord_x, coord_y).status == state::susceptible)
+                            if (new_board(coord_y, coord_x).status == state::susceptible)
                             {
-                                // controllare che è veramente casuale
-                                std::random_device e;
-                                std::uniform_real_distribution<double> distribution(0, 1);
-                                double random = distribution(e);
+                                double random = distribution_for_probability(e);
 
                                 if (random < infection_constants::infection_probability)
-                                    new_board(coord_x, coord_y).status = state::infected;
+                                    new_board(coord_y, coord_x).status = state::infected;
                             }
                         }
                     }
                 }
 
-                // Clock progression
-                if (new_board(row, column).clock < infection_constants::infection_time) //Change time to a variable
+                // Questo if else aumenta la variabile clock fino a un valore definito, dopodiché decide se la cellula diventa immune o muore
+                if (new_board(row, column).clock < infection_constants::infection_time)
                 {
                     new_board(row, column).clock++;
                 }
                 else
                 {
-                    std::random_device a;
-                    std::uniform_real_distribution<double> distr(0, 1);
-                    double nrand = distr(a);
-                    if (nrand <= infection_constants::mortality_weight)
+                    double nrand = distribution_for_probability(e);
+                    if (nrand <= infection_constants::mortality_rate)
                     {
                         new_board(row, column).status = state::dead;
                     }
@@ -69,29 +69,24 @@ Board evolve(Board const& board)
                 }
             }
 
-            //movement
-            //NOTA: occorre dare la possibilità di scegliere quale utilizzare
+            //Movimento delle cellule
             if ((current_cell.status != state::non_existant) && (current_cell.status != state::dead))
             {
                 int failure_counter = 0;
 
-                std::random_device e;
-                auto distribution = std::uniform_int_distribution<int>(-1, 1);
-
-                while (failure_counter != 8)
+                while (failure_counter != 1) // Se si mette un valore maggiore di uno il movimento appare più casuale. La performance del programma però ne risente quando ci sono molte cellule
                 {
-                    //srand(time(0)); // Questo risolve il problema però non è veramente random
                     std::random_device e;
 
-                    new_board(row, column).Vx = distribution(e);
-                    new_board(row, column).Vy = distribution(e);
+                    new_board(row, column).Vx = velocity_distribution(e);
+                    new_board(row, column).Vy = velocity_distribution(e);
 
-                    //for readability
+                    //Per leggibilità
                     int vx = new_board(row, column).Vx;
                     int vy = new_board(row, column).Vy;
 
 
-                    //Boundary conditions
+                    //Ribalta la velocità della cellula se va fuori la griglia
                     if (row + vx >= new_board.get_size() || row + vx < 0)
                         vx = -vx;
 
@@ -101,7 +96,7 @@ Board evolve(Board const& board)
                     if (new_board(row + vx, column + vy).status == state::non_existant)
                     {
                         new_board(row + vx, column + vy) = new_board(row, column);
-                        new_board(row, column) = Cell(state::non_existant); // Assicurarsi che funzioni questa riassegnazione
+                        new_board(row, column) = Cell(state::non_existant);
 
                         break;
                     }
