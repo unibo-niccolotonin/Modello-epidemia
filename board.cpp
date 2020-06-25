c3fe30f2dc121ea9807ace701fa4394a83ee38d8
unibo-elenaT <elena.tabakovic@studio.unibo.it>
2020-06-25 14:48:25 +0200

    utilizzato Clang Format

--- board.cpp
+++ board.cpp
@@ -1,22 +1,19 @@
 #include "board.hpp"
 #include <cassert>
 #include <iostream>
-#include <vector>
 #include <random>
+#include <vector>
 
 float Board::infection_probability = 0.2;
-int Board::infection_radius = 1 ;
+int Board::infection_radius = 1;
 int Board::infection_time = 30;
 float Board::mortality_rate = 0.3;
 
 Cell Board::null_cell_ = Cell(NON_EXISTANT);
 
-bool Board::insertCell(int r, int c) 
-{
-  if (r >= 0 && c >= 0 && r < size_ && c < size_) 
-  {
-    if (board_[r * size_ + c].status == NON_EXISTANT) 
-    {
+bool Board::insertCell(int r, int c) {
+  if (r >= 0 && c >= 0 && r < size_ && c < size_) {
+    if (board_[r * size_ + c].status == NON_EXISTANT) {
       board_[r * size_ + c].status = SUSCEPTIBLE;
       return true;
     }
@@ -24,12 +21,9 @@ bool Board::insertCell(int r, int c)
   return false;
 };
 
-bool Board::insertCell(int r, int c, state cell_state) 
-{
-  if (r >= 0 && c >= 0 && r < size_ && c < size_) 
-  {
-    if (board_[r * size_ + c].status == NON_EXISTANT) 
-    {
+bool Board::insertCell(int r, int c, state cell_state) {
+  if (r >= 0 && c >= 0 && r < size_ && c < size_) {
+    if (board_[r * size_ + c].status == NON_EXISTANT) {
       board_[r * size_ + c].status = cell_state;
       return true;
     } else
@@ -38,47 +32,34 @@ bool Board::insertCell(int r, int c, state cell_state)
     return false;
 }
 
-void Board::fill_board(int n, state cell_state)
-{
-    assert (n < size_ * size_);
+void Board::fill_board(int n) {
+  assert(n < size_ * size_);
 
-    std::random_device rd;
-    std::mt19937 dev(rd());
-    std::uniform_int_distribution<int> random_coord(0, size_ - 1);
+  std::random_device rd;
+  std::mt19937 dev(rd());
+  std::uniform_int_distribution<int> random_coord(0, size_ - 1);
 
-    int i = 0;
-    int failure_counter = 0; //Questa variabile controlla che il while loop non continui per un tempo troppo lungo
-    while (i < n)
-    {
-        int coord_x = random_coord(dev);
-        int coord_y = random_coord(dev);
+  int i = 0;
+  while (i < n) {
+    int coord_x = random_coord(dev);
+    int coord_y = random_coord(dev);
 
-        if (board_[coord_y * size_ + coord_x].status == NON_EXISTANT)
-        {
-            this->insertCell(coord_y, coord_x, cell_state);
-            i++;
-            failure_counter = 0;
-        } else
-        {
-            assert(failure_counter < 1000);
-            failure_counter++;
-        }
+    if (board_[coord_y * size_ + coord_x].status == NON_EXISTANT) {
+      this->insertCell(coord_y, coord_x);
+      i++;
     }
+  }
 }
 
-Cell &Board::operator()(int y, int x) 
-{
-  if (x >= 0 && x < size_ && y >= 0 && y < size_) 
-  {
+Cell &Board::operator()(int y, int x) {
+  if (x >= 0 && x < size_ && y >= 0 && y < size_) {
     return board_[y * size_ + x];
   } else
     return null_cell_;
 }
 
-Cell Board::operator()(int y, int x) const 
-{
-  if (x >= 0 && x < size_ && y >= 0 && y < size_) 
-  {
+Cell Board::operator()(int y, int x) const {
+  if (x >= 0 && x < size_ && y >= 0 && y < size_) {
     return board_[y * size_ + x];
   } else
     return Cell(NON_EXISTANT);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "board.hpp"
-#include "evolve.hpp"
 #include "draw.hpp"
+#include "evolve.hpp"
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
@@ -11,105 +11,81 @@
 #include <thread>
 #include <vector>
 
-template<typename T>
-void loop_of_failure(T& var, int max_value)
-{
-    while (std::cin.fail() || var < 0 || var > max_value)
-    { //verifica che il valore inserito non sia una stringa e che non sia negativo
-        std::cout << "input non valido, inserire un valore corretto: ";
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cin >> var;
-    }
+template <typename T> void loop_of_failure(T &var, int max_value) {
+  while (std::cin.fail() || var < 0 ||
+         var > max_value) { // verifica che il valore inserito non sia una
+                            // stringa e che non sia negativo
+    std::cout << "input non valido, inserire un valore corretto: ";
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin >> var;
+  }
 }
 
-void set_values(Board& board)
-{
-     //questa funzione serve ad inserire le variabili dell'infezione
-    std::cout << "inserimento costanti:\n";
-    std::cout << "inserire il raggio di infezione: ";
-    std::cin >> Board::infection_radius;
+void set_infection_values(Board const &board) {
+  // questa funzione serve ad inserire le variabili dell'infezione
+  std::cout << "inserimento costanti:\n";
+  std::cout << "inserire il raggio di infezione: ";
+  std::cin >> Board::infection_radius;
 
-    loop_of_failure(Board::infection_radius, board.get_size());
+  loop_of_failure(Board::infection_radius, board.get_size());
 
-    std::cout << "\ninserire la probabilità di infezione fra 0 e 1: ";
-    std::cin >> Board::infection_probability;
+  std::cout << "\ninserire la probabilità di infezione: ";
+  std::cin >> Board::infection_probability;
 
-    loop_of_failure(Board::infection_probability, 1);
+  loop_of_failure(Board::infection_probability, 1);
 
-    std::cout << "\ninserire il tempo di durata dell'infezione: ";
-    std::cin >> Board::infection_time;
+  std::cout << "\ninserire il tempo di durata dell'infezione: ";
+  std::cin >> Board::infection_time;
 
-    loop_of_failure(Board::infection_time, 1000000);
+  loop_of_failure(Board::infection_time, 1000000);
 
-    std::cout << "\ninserire la probabilità di morte di una cellula infetta fra 0 e 1: ";
-    std::cin >> Board::mortality_rate;
+  std::cout << "\ninserire la probabilità di morte di una cellula infetta: ";
+  std::cin >> Board::mortality_rate;
 
-    loop_of_failure(Board::mortality_rate, 1);
-
-    int n_cells;
-    std::cout << "\ninserire il numero di cellule suscettibili nella griglia, non maggiore della metà delle celle nella board: ";
-    std::cin >> n_cells;
-    loop_of_failure(n_cells, board.get_size() * board.get_size() / 2);
-
-    board.fill_board(n_cells, SUSCEPTIBLE);
-
-    std::cout << "\ninserire il numero di cellule infette nella griglia, non maggiore della lunghezza del lato della griglia: ";
-    std::cin >> n_cells;
-    loop_of_failure(n_cells, board.get_size());
-
-    board.fill_board(n_cells, INFECTED);
+  loop_of_failure(Board::mortality_rate, 1);
 }
 
 int main() {
 
-  Board board(100);
+  Board board(50);
 
-  set_values(board);
+  set_infection_values(board);
+
+  board.fill_board(100);
 
   std::cout << "Suscettibili   Infetti   Rimossi\n";
-    
+
   // Contiene le varie altezze delle colonne per disegnare il grafico della
   // curva d'infezione
-  std::vector<std::array<int, 4> > graph_columns_heights;
+  std::vector<std::array<int, 4>> graph_columns_heights;
 
-  board.cell_length = 5.0f;
-  board.graph_column_width = 2.0f;
+  board.cell_length = 10.0f;
+  board.graph_column_width = 4.0f;
   board.graph_height = 300.0f;
 
-  sf::RenderWindow window(sf::VideoMode(1000, 1000), "Simulazione infezione", sf::Style::Titlebar | sf::Style::Close);
+  sf::RenderWindow window(sf::VideoMode(1000, 1000), "Simulazione infezione",
+                          sf::Style::Titlebar | sf::Style::Close);
   window.setFramerateLimit(60);
 
-  state mouse_cell_state = SUSCEPTIBLE;
-
   sf::Event event;
-  while (window.isOpen()) 
-  {
-    while (window.pollEvent(event)) 
-    {
+  while (window.isOpen()) {
+    while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
-      if (event.type == sf::Event::MouseButtonPressed) // aggiunge una cellula quando si clicca la finestra
+      if (event.type ==
+          sf::Event::MouseButtonPressed) // aggiunge una cellula quando si
+                                         // clicca la finestra
       {
         int x = std::floor(event.mouseButton.x / board.cell_length);
         int y = std::floor(event.mouseButton.y / board.cell_length);
 
-        board.insertCell(y, x, mouse_cell_state);
-      }
-      if (event.type == sf::Event::KeyPressed)
-      {
-        if (event.key.code == sf::Keyboard::I)
-        {
-          mouse_cell_state = INFECTED;
-        } else if (event.key.code == sf::Keyboard::S)
-        {
-          mouse_cell_state = SUSCEPTIBLE;
-        }         
+        board.insertCell(y, x);
       }
     }
-      
+
     board = evolve(board);
-      
+
     window.clear();
     draw(window, board, graph_columns_heights);
     window.display();
